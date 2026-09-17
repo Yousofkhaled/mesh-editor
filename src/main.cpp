521afbf774ab2e89651c84924007745779fb2c08
Yousof Khaled <yk849754@gmail.com>
2026-09-18 02:08:18 +0300

    add zoom tool

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@
 #define RAYGUI_IMPLEMENTATION
 #include "external/raygui.h"
 
+#include "algorithm"
+
 // constants
 float global_sensetivity = 0.01;
 
 typedef Vector2 vec2;
 typedef Vector3 vec3;
 
-void tool_pan_input(Camera3D& camera, Vector3& forward_vector);
+void tool_pan_input(Camera3D& camera, Vector3 forward_vector);
+void tool_zoom_input(Camera3D& camera, Vector3 forward_vector);
 
 int main()
 {
@@ -22,8 +25,7 @@ int main()
     SetTargetFPS(60);
 
     Camera3D camera = { 0 };
-    Vector3 forward_vector = {0, 0, -10};
-    Vector3 forward_vector_normalized = Vector3Normalize(forward_vector);
+    Vector3 forward_vector = {0, 0, -1};
     {
         camera.position = (Vector3){ 0.0f, 0.0f, 10.0f };   
         camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
@@ -38,10 +40,8 @@ int main()
     {
         // Update
         //----------------------------------------------------------------------------------
-        tool_pan_input(camera, forward_vector_normalized);
-
-        // update target after positions changes.
-        camera.target = camera.position + forward_vector;
+        tool_pan_input(camera, forward_vector);
+        tool_zoom_input(camera, forward_vector);
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -60,7 +60,7 @@ int main()
     return 0;
 }
 
-void tool_pan_input(Camera3D& camera, Vector3& forward_vector)
+void tool_pan_input(Camera3D& camera, Vector3 forward_vector)
 {
     if ((IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)) &&
         IsMouseButtonDown(MOUSE_BUTTON_LEFT))
@@ -73,5 +73,20 @@ void tool_pan_input(Camera3D& camera, Vector3& forward_vector)
 
         camera.position += Vector3Scale(right, -delta.x * global_sensetivity);
         camera.position += Vector3Scale(up, delta.y * global_sensetivity);
+        camera.target = camera.position + forward_vector;
+    }
+}
+
+void tool_zoom_input(Camera3D& camera, Vector3 forward_vector)
+{
+    if ((IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)))
+    {
+        auto wheel = GetMouseWheelMoveV();
+
+        if (wheel.y != 0.0f)
+        {
+            camera.position -= Vector3Scale(forward_vector, wheel.y);
+            camera.target = camera.position + forward_vector;
+        }
     }
 }
